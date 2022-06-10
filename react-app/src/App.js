3d845cccba6e276ea405c982e81afb0704dcdd55
Archie Torres <xtremesama@gmail.com>
2022-06-10 06:21:03 -0700

    Implemented hooks for emotions

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,17 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-
-const [firstCity, secondCity] = ["Tokyo", "Tahoe City", "Bend"];
-
-console.log(firstCity);
-console.log(secondCity);
+import { useState, useEffect } from "react";
 
 function App({ library }) {
-  return (
-    <div className="App">
-      <h1>Hello from {library}</h1>
-    </div>
-  );
+    const [emotion, setEmotion] = useState("happy");
+    const [secondary, setSecondary] = useState("tired");
+
+    useEffect(() => {
+        console.log(`It's ${emotion} right now`)
+    }, [emotion, secondary]);
+
+    useEffect(() => {
+        console.log(`It's ${secondary} right now`)
+    }, [secondary]);
+
+    return (
+        <div className="App">
+            <h1>Current emotion is {emotion}</h1>
+            <button onClick={() => setEmotion("sad")}>Sad</button>
+            <button onClick={() => setEmotion("excited")}>Excited</button>
+            <h2>Current secondary emotion is {secondary}</h2>
+            <button onClick={() => setSecondary("grateful")}>Grateful</button>
+        </div>
+    );
 }
 
 export default App;
