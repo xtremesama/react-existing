631d10a768d931736e34b0412e0985e80a4aa192
Archie Torres <xtremesama@gmail.com>
2022-06-10 05:47:33 -0700

    Added NodeJS

---
+++ react-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App(props) {
+  return (
+    <div className="App">
+      <h1>Hello from {props.library}</h1>
+    </div>
+  );
+}
+
+export default App;
