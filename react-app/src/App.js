b68080bef4e08880233f12f0f23d3248c90b2f0e
Archie Torres <xtremesama@gmail.com>
2022-06-10 05:52:44 -0700

    Destructuring example

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App(props) {
+const [firstCity, secondCity] = ["Tokyo", "Tahoe City", "Bend"];
+
+console.log(firstCity);
+console.log(secondCity);
+
+function App({ library }) {
   return (
     <div className="App">
-      <h1>Hello from {props.library}</h1>
+      <h1>Hello from {library}</h1>
     </div>
   );
 }
